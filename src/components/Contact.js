eea09f8545fffc2cd68b32dc43ec942fe26c97c4
lucianocloudbr <luciano.cloudbr@gmail.com>
2024-05-23 16:25:48 -0300

    Primeiro

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Página de Contato</h1>
+      <p>Entre em contato conosco .</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Minha página</h1>
+      <p>Bem-vindo à pagina inicial!</p>
+    </div>
+  );
+};
+
+export default Home;
